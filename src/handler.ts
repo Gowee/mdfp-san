bb31b595e368eaa298b1a849be7f9070e61d0d25
Hung-I Wang <whygowe@gmail.com>
2020-07-02 13:34:49 +0800

    Boom

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,22 @@
-export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+import { cached } from './utils';
+
+export async function handleEvent(event: FetchEvent): Promise<Response> {
+  const request = event.request;
+  const url = new URL(request.url);
+  switch (url.pathname) {
+    case '/':
+      return new Response(`request method: ${request.method}`);
+      break;
+    case '/query':
+      return await cached(handleQuery)(event);
+      break;
+    default:
+      return new Response(`Resource Not Found at the endpoint ${url.pathname}`, { status: 404 });
+  }
+}
+
+async function handleQuery(request: Request): Promise<Response> {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  
 }
