1930d46d81a9e6a374cb06a27ed089b64764bd25
Hung-I Wang <whygowe@gmail.com>
2020-07-03 12:47:06 +0800

    npm run format

--- src/handler.ts
+++ src/handler.ts
@@ -1,50 +1,58 @@
-import { cached, getDomainBase } from './utils';
-import * as Crtsh from './crtsh';
+import { cached, getDomainBase } from './utils'
+import * as Crtsh from './crtsh'
 
 export async function handleEvent(event: FetchEvent): Promise<Response> {
-  const request = event.request;
-  const url = new URL(request.url);
+  const request = event.request
+  const url = new URL(request.url)
   switch (url.pathname) {
     case '/':
-      return new Response(`request method: ${request.method}`);
-      break;
+      return new Response(`request method: ${request.method}`)
+      break
     case '/query':
-      return await cached(handleQuery)(event);
-      break;
+      return await cached(handleQuery)(event)
+      break
     default:
-      return new Response(`Resource Not Found at the endpoint ${url.pathname}`, { status: 404 });
+      return new Response(
+        `Resource Not Found at the endpoint ${url.pathname}`,
+        { status: 404 },
+      )
   }
 }
 
 async function handleQuery(request: Request): Promise<Response> {
-  const url = new URL(request.url);
-  const q = url.searchParams.get("q");
+  const url = new URL(request.url)
+  const q = url.searchParams.get('q')
   if (q === null) {
-    throw Error("");
+    throw Error('')
   }
 
-  const seenCerts = new Set<number>();
-  const seenQs = new Set<string>([q]);
+  const seenCerts = new Set<number>()
+  const seenQs = new Set<string>([q])
 
-  const sans = new Set();
+  const sans = new Set()
 
-  const entries = await Crtsh.searchCT(q);
+  const entries = await Crtsh.searchCT(q)
   for (const entry of entries.slice(0, 15)) {
     if (seenCerts.has(entry.id)) {
       continue
     }
-    seenCerts.add(entry.id);
+    seenCerts.add(entry.id)
 
-    const cert = await Crtsh.fetchCertInfo(entry.id);
-    console.log(cert);
+    const cert = await Crtsh.fetchCertInfo(entry.id)
+    console.log(cert)
     if (cert === null) {
-      continue;
+      continue
     }
-    sans.add(getDomainBase(cert.commonName));
+    sans.add(getDomainBase(cert.commonName))
+    console.log('CN', cert.commonName)
     for (const san of cert.subjectAlternativeNames) {
-      sans.add(getDomainBase(san));
+      sans.add(getDomainBase(san))
     }
+    console.log('done')
   }
 
-  return new Response(JSON.stringify({ domains: Array.from(sans), _t: new Date() }), { headers: { 'Content-Type': "application/json" } });
+  return new Response(
+    JSON.stringify({ domains: Array.from(sans), _t: new Date() }),
+    { headers: { 'Content-Type': 'application/json' } },
+  )
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,32 +1,29 @@
-import psl from './psl';
+import psl from './psl'
 
 export function cached(func: (request: Request) => Promise<Response>) {
-    // Ref: https://github.com/cloudflare/template-registry/blob/f2a21ff87a4f9c60ce1d426e9e8d2e6807b786fd/templates/javascript/cache_api.js#L9
-    const cache = caches.default;
-    async function cachedFunc(event: FetchEvent) {
-        const request = event.request;
-        let response = await cache.match(request);
-        if (!response) {
-            response = await func(request);
-            event.waitUntil(cache.put(request, response.clone()));
-        }
-        return response;
+  // Ref: https://github.com/cloudflare/template-registry/blob/f2a21ff87a4f9c60ce1d426e9e8d2e6807b786fd/templates/javascript/cache_api.js#L9
+  const cache = caches.default
+  async function cachedFunc(event: FetchEvent) {
+    const request = event.request
+    let response = await cache.match(request)
+    if (!response) {
+      response = await func(request)
+      event.waitUntil(cache.put(request, response.clone()))
     }
-    return cachedFunc;
+    return response
+  }
+  return cachedFunc
 }
 
-console.log(psl);
-
 export function getDomainBase(domain: string): string {
-    while (domain.startsWith("*.")) { // although *.*. is invalid according to CA/B guidelines
-        domain = domain.slice(2);
+  const parts = domain.split('.')
+  let l = 0,
+    r = 0
+  while ((r = domain.indexOf('.', l)) !== -1) {
+    if (psl.has(domain.slice(r + 1))) {
+      break
     }
-    let ls = 0; // longest suffix
-    for (const suffix of psl) {
-        if (domain.endsWith(suffix)) {
-            ls = Math.max(suffix.split(".").length, ls);
-        }
-    }
-    const parts = domain.split(".");
-    return parts.slice(parts.length - ls - 1).join(".");
+    l = r + 1
+  }
+  return domain.slice(l)
 }
